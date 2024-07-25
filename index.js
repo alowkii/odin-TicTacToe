ac39f4f27427d57a4846c19c7e9c015b7052c00a
fellowpandit <paalok333@gmail.com>
2024-07-25 23:53:21 +0530

    Computer Move finally added.

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 var count = 0;
 var winner = "";
+var playerCard = "";
+var computerCard = "";
+
+function computerMove() {
+  var emptyCells = [];
+  var cells = document.querySelectorAll('.cell');
+  for(var i = 0; i < 9; i++) {
+    if(cells[i].innerHTML === '') {
+      emptyCells.push(i);
+    }
+  }
+
+  if(emptyCells.length === 0) {
+    return;
+  }
+  
+  var randomCell = Math.floor(Math.random() * emptyCells.length);
+  putMarkAt(emptyCells[randomCell]);
+  setTimeout(checkWinner, 200);
+}
 
 function cellClick(num) {
-  
+  if(getPlayerCard() === null) {
+    alert('Player must select a mark');
+    return;
+  }
+
+  cellSpace = document.getElementById(`c${num}`);
+  if(cellSpace.innerHTML !== '') {
+    return;
+  }
+  putMarkAt(num);
+  setTimeout(checkWinner, 200);
+  computerMove();
 }
 
 function putMarkAt(cellNo) {
@@ -18,49 +49,8 @@ function putMarkAt(cellNo) {
   }
 }
 
-function checkWinner() {
-  var c0 = document.getElementById('c0').innerHTML;
-  var c1 = document.getElementById('c1').innerHTML;
-  var c2 = document.getElementById('c2').innerHTML;
-  var c3 = document.getElementById('c3').innerHTML;
-  var c4 = document.getElementById('c4').innerHTML;
-  var c5 = document.getElementById('c5').innerHTML;
-  var c6 = document.getElementById('c6').innerHTML;
-  var c7 = document.getElementById('c7').innerHTML;
-  var c8 = document.getElementById('c8').innerHTML;
-
-  if(c0 === c1 && c1 === c2 && c0 !== '') {
-    announceWinner(c0);
-    resetGame();
-  } else if(c3 === c4 && c4 === c5 && c3 !== '') {
-    announceWinner(c3);
-    resetGame();
-  } else if(c6 === c7 && c7 === c8 && c6 !== '') {
-    announceWinner(c6);
-    resetGame();
-  } else if(c0 === c3 && c3 === c6 && c0 !== '') {
-    announceWinner(c0);
-    resetGame();
-  } else if(c1 === c4 && c4 === c7 && c1 !== '') {
-    announceWinner(c1);
-    resetGame();
-  } else if(c2 === c5 && c5 === c8 && c2 !== '') {
-    announceWinner(c2);
-    resetGame();
-  } else if(c0 === c4 && c4 === c8 && c0 !== '') {
-    announceWinner(c0);
-    resetGame();
-  } else if(c2 === c4 && c4 === c6 && c2 !== '') {
-    announceWinner(c2);
-    resetGame();
-  } else if(count === 9) {
-    announceWinner('draw');
-    resetGame();
-  }
-}
-
 function setPlayerCard(num) {
-  var playerCard = document.querySelector(`.btn${num}`);
+  playerCard = document.querySelector(`.btn${num}`);
   playerCard.setAttribute('id', 'playerCard');
   playerCard.style.backgroundColor = '#555';
   if(num === 0) {
@@ -88,20 +78,56 @@ function announceWinner(winner) {
   if(winner === 'draw') {
     alert('It\'s a draw!');
   } else {
-    if(getPlayerCard().innerHTML === 'X'){
+    if(getPlayerCard().innerHTML.trim() == winner.trim()){
+      winner = 'player';
+      alert(`Player wins!`);
+    }else{
+      winner = 'computer';
+      alert(`Computer wins!`);
     }
-    alert(`Player ${winner} wins!`);
   }
-  setScore();
-  startGame();
+  setScore(winner);
+  resetBoard();
 }
 
-function setScore() {
+function checkWinner() {
+  var c0 = document.getElementById('c0').innerHTML;
+  var c1 = document.getElementById('c1').innerHTML;
+  var c2 = document.getElementById('c2').innerHTML;
+  var c3 = document.getElementById('c3').innerHTML;
+  var c4 = document.getElementById('c4').innerHTML;
+  var c5 = document.getElementById('c5').innerHTML;
+  var c6 = document.getElementById('c6').innerHTML;
+  var c7 = document.getElementById('c7').innerHTML;
+  var c8 = document.getElementById('c8').innerHTML;
+
+  if(c0 === c1 && c1 === c2 && c0 !== '') {
+    announceWinner(c0);
+  } else if(c3 === c4 && c4 === c5 && c3 !== '') {
+    announceWinner(c3);
+  } else if(c6 === c7 && c7 === c8 && c6 !== '') {
+    announceWinner(c6);
+  } else if(c0 === c3 && c3 === c6 && c0 !== '') {
+    announceWinner(c0);
+  } else if(c1 === c4 && c4 === c7 && c1 !== '') {
+    announceWinner(c1);
+  } else if(c2 === c5 && c5 === c8 && c2 !== '') {
+    announceWinner(c2);
+  } else if(c0 === c4 && c4 === c8 && c0 !== '') {
+    announceWinner(c0);
+  } else if(c2 === c4 && c4 === c6 && c2 !== '') {
+    announceWinner(c2);
+  } else if(count === 9) {
+    announceWinner('draw');
+  }
+}
+
+function setScore(winner) {
   var playerScore = document.getElementById('playerScore');
   var compScore = document.getElementById('compScore');
-  if(winner === 'player') {
+  if(winner == 'player') {
     playerScore.innerHTML = parseInt(playerScore.innerHTML) + 1;
-  } else {
+  } else if(winner == 'computer') {
     compScore.innerHTML = parseInt(compScore.innerHTML) + 1;
   }
 }
@@ -117,9 +143,15 @@ function resetBoard() {
 
 function startGame() {
   if(getPlayerCard() === null) {
-    alert('Please select a card');
+    alert('Please select a card!');
     return;
   }
+
+  if(count === 0) {
+    alert('Please make a move!');
+    return;
+  }
+
   setScore();
   resetBoard();
 }
